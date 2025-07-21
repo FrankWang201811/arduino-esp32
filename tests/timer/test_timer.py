80bba429dd037dcf01b29ec9937e9011efdbc5f4
FrankTakumi <frankwangtakumi@gmail.com>
2025-07-21 17:58:31 -0600

    tests: skip when pytest_embedded not available

--- tests/timer/test_timer.py
+++ tests/timer/test_timer.py
@@ -1,2 +1,7 @@
+import pytest
+
+pytest.importorskip("pytest_embedded")
+
+
 def test_timer(dut):
     dut.expect_unity_test_output(timeout=240)
--- tests/unity/test_unity.py
+++ tests/unity/test_unity.py
@@ -1,2 +1,7 @@
+import pytest
+
+pytest.importorskip("pytest_embedded")
+
+
 def test_unity(dut):
     dut.expect_unity_test_output(timeout=240)
